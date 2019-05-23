7dfc8d14a3900baea03b199f79b6db7881c3ff35
Rajeesh CV <rajeesh.cv@devfactory.com>
2019-05-23 14:42:48 +0530

    add support for VS Code data persistence

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,13 @@
 
 set -e
 
-# VS remote attach script fails if folder exists
-# if [ ! -L "/root/.vscode-remote" ]; then
-#   # Create Symlink for VSCode data
-#   ln -s /data/.vscode-remote /root/.vscode-remote
-# fi
+# Setup VS Code data directory as a symlink of directory under /data
+# so that user no need to install extensions on every DevSpace restarts.
+mkdir -p /data/.vscode-remote
+if [ ! -L "/root/.vscode-remote" ]; then
+  # Create Symlink for VSCode data
+  ln -s /data/.vscode-remote /root/.vscode-remote
+fi
 
 # Start SSH server
 /usr/sbin/sshd -D &
